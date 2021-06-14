998f800d1d0d9204ffb8dfd6daba3406e0232be5
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2021-06-14 23:12:59 +0200

    Adapted Prefabs and Changed Shader

--- Assets/_Prefabs/Materials/Apple02_MPI01/export.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-def export_all_fbx(exportFolder):
-    objects = bpy.data.objects
-    for object in objects:
-        bpy.ops.object.select_all(action='DESELECT')
-        object.select = True
-        exportName = exportFolder + object.name + '.fbx'
-        bpy.ops.export_scene.fbx(filepath=exportName, use_selection=True)
