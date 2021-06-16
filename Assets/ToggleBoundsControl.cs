759d9f4446c7923d91e4f4c35ee735ad28dccf52
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2021-06-16 17:26:33 +0200

    öasldk jf

--- Assets/ToggleBoundsControl.cs
+++ Assets/ToggleBoundsControl.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class ToggleBoundsControl : MonoBehaviour
 {
     private int DefaultLayer;
-    private int ResizePlayarea; 
+    private int ResizePlayarea;
+    private GameObject[] interactionObjects; 
+   
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +16,43 @@ public class ToggleBoundsControl : MonoBehaviour
         DefaultLayer = 0;
         ResizePlayarea = 6; 
 
-        Physics.IgnoreLayerCollision(DefaultLayer, ResizePlayarea);  
+        Physics.IgnoreLayerCollision(DefaultLayer, ResizePlayarea);
+        interactionObjects = GameObject.FindGameObjectsWithTag("InteractionObject"); 
+         
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            _ActivateResize();
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            _DeactivateResize(); 
+        }
     }
 
     public void _DeactivateResize()
     {
         GetComponent<BoxCollider>().enabled = false;
-        GetComponent<BoundsControl>().Active = false; 
+        GetComponent<BoundsControl>().Active = false;
+
+        foreach (GameObject obj in interactionObjects)
+        {
+            obj.AddComponent<Rigidbody>(); 
+        }
     }
 
     public void _ActivateResize()
     {
         GetComponent<BoxCollider>().enabled = true;
         GetComponent<BoundsControl>().Active = true;
+
+        foreach (GameObject obj in interactionObjects)
+        {
+            Destroy(obj.GetComponent<Rigidbody>()); 
+        }
+       
     }
 }
